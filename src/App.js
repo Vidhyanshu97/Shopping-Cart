1c590d561c6c8e79bd0928b9c37e4930783ff1ab
VidhyanshuMishra <vidhyanshumishra377@gmail.com>
2023-06-25 15:30:53 +0530

    Shopping Cart commmit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Cart from "./components/Cart.js";
+
+const App = () => {
+  return (
+    <div>
+      <h1><Cart/></h1>
+    </div>
+  );
+}
+
+export default App;
